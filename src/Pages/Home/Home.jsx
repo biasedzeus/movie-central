7748af8511b3b57695b4e421e316968f7243a1aa
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-10-11 19:27:23 +0530

    movieDetail page added and Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,40 +1,104 @@
 import React, { useEffect, useState } from "react";
-import "./home.css";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import MovieGallery from "../../components/MovieGallery";
 import axios from "axios";
 import HomeSkeleton from "../../components/skeleton/HomeSkeleton";
 import Header from "../../components/Header";
-
-const BASE_URL = "https://movie-task.vercel.app/api/popular?page=1";
+import SearchBar from "../../components/SearchBar";
+import Filter from "../../components/Filter";
 
 export default function Home() {
   const [movieList, setMovieList] = useState([]);
+  const [displayList, setDisplayData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [filters, setFilters] = useState(0);
+  const [resultsFound, setResultsFound] = useState(true);
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [filters]);
+
+ 
+
+
+  const yearReleasedList = [
+    ...new Set(displayList.map((movie) => movie.release_date)),
+  ];
+  const newList = [
+    ...new Set(yearReleasedList.map((item) => new Date(item).getUTCMonth())),
+  ].sort((a, b) => a - b);
+
+  console.log("yr", yearReleasedList);
+  console.log("filter", filters);
+
+  function handleChange(e) {
+    setFilters(e.target.value);
+  }
+
+  function applyFilter() {
+    let updatedList = movieList;
+
+    if (filters) {
+      updatedList = updatedList.filter((data) => {
+        return filters === new Date(data.release_date).getMonth();
+      });
+    }
+    setMovieList(updatedList);
+    !updatedList.length ? setResultsFound(false) : setResultsFound(true);
+  }
+
+  function clearFilters() {
+    setMovieList(displayList);
+    setFilters(0);
+  }
 
   async function getData() {
     try {
-      const { data } = await axios.get(`${import.meta.env.VITE_BASE_URL}popular?page=1`);
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}popular?page=1`
+      );
       console.log(data.data?.results);
       setMovieList(data.data?.results);
-      // setIsLoading(false);
-      setTimeout(() =>{
-        setIsLoading(false)
-      },3000)
+      setDisplayData(data?.data?.results);
+      setIsLoading(false);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
     } catch (error) {
+      setIsLoading(false);
+      setError(error);
       console.log("error", error);
     }
   }
+  if (error?.code)
+    return (
+      <Stack alignItems="center" justifyContent="center" height="100vh">
+        <Typography variant="h5">
+          Something Went Wrong. Please Try again Later
+        </Typography>
+      </Stack>
+    );
+
   return (
     <Box minHeight={"100vh"}>
-      <Header/>
-      {isLoading ? <HomeSkeleton/> :  <MovieGallery movies={movieList} /> }
-     
+      <Header />
+      <SearchBar />
+      <Filter
+        filters={filters}
+        newList={newList}
+        applyFilters={applyFilter}
+        yearList={yearReleasedList}
+        handleChange={handleChange}
+        clearFilters={clearFilters}
+      />
+      {isLoading ? (
+        <HomeSkeleton />
+      ) : resultsFound ? (
+        <MovieGallery movies={movieList} />
+      ) : (
+        <Typography>No results</Typography>
+      )}
     </Box>
   );
 }
--- src/Pages/movieDetail/MovieDetail.jsx
+++ src/Pages/movieDetail/MovieDetail.jsx
@@ -1,91 +1,162 @@
-import React, {useEffect, useState} from "react"
-import { useParams } from "react-router-dom"
-import { Stack } from "@mui/material";
-import "./movies.css"
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import MovieCard from "../../components/MovieCard";
+import styled from "@emotion/styled";
+import MovieDetailSkeleton from "../../components/skeleton/MovieDetailSkeleton";
+import { FcRating, FcLike } from "react-icons/fc";
+import { format } from "date-fns/esm";
+import { MdMovieCreation } from "react-icons/md";
+import axios from "axios";
 
-const Movie = () => {
-    const [currentMovieDetail, setMovie] = useState()
-    const { id } = useParams()
+const BackDropImage = styled("img")({
+  width: "100%",
+  minHeight: "80% !important",
+});
+const MovieDetail = () => {
+  const [currentMovieDetail, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
 
-    useEffect(() => {
-        getData()
-        window.scrollTo(0,0)
-    }, [])
+  useEffect(() => {
+    getData();
+    window.scrollTo(0, 0);
+  }, []);
 
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovie(data))
-    }
+  const  getData = async () => {
+    const {data} = await axios.get(`${import.meta.env.VITE_BASE_URL}movie?movieId=${id}`);
+    setMovie(data?.data);
+    setIsLoading(false);
+  };
 
-    return (
-        <div className="movie">
-            <div className="movie__intro">
-                <img className="movie__backdrop" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} />
-            </div>
-            <div className="movie__detail">
-                <div className="movie__detailLeft">
-                    <div className="movie__posterBox">
-                        <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
-                    </div>
-                </div>
-                <div className="movie__detailRight">
-                    <div className="movie__detailRightTop">
-                        <div className="movie__name">{currentMovieDetail ? currentMovieDetail.original_title : ""}</div>
-                        <div className="movie__tagline">{currentMovieDetail ? currentMovieDetail.tagline : ""}</div>
-                        <div className="movie__rating">
-                            {currentMovieDetail ? currentMovieDetail.vote_average: ""} <i class="fas fa-star" />
-                            <span className="movie__voteCount">{currentMovieDetail ? "(" + currentMovieDetail.vote_count + ") votes" : ""}</span>
-                        </div>  
-                        <div className="movie__runtime">{currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}</div>
-                        <div className="movie__releaseDate">{currentMovieDetail ? "Release date: " + currentMovieDetail.release_date : ""}</div>
-                        <div className="movie__genres">
-                            {
-                                currentMovieDetail && currentMovieDetail.genres
-                                ? 
-                                currentMovieDetail.genres.map(genre => (
-                                    <><span className="movie__genre" id={genre.id}>{genre.name}</span></>
-                                )) 
-                                : 
-                                ""
-                            }
-                        </div>
-                    </div>
-                    <div className="movie__detailRightBottom">
-                        <div className="synopsisText">Synopsis</div>
-                        <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
-                    </div>
-                    
-                </div>
-            </div>
-            <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
-                {
-                    currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
-            <div className="movie__heading">Production companies</div>
-            <div className="movie__production">
-                {
-                    currentMovieDetail && currentMovieDetail.production_companies && currentMovieDetail.production_companies.map(company => (
-                        <>
-                            {
-                                company.logo_path 
-                                && 
-                                <span className="productionCompanyImage">
-                                    <img className="movie__productionComapany" src={"https://image.tmdb.org/t/p/original" + company.logo_path} />
-                                    <span>{company.name}</span>
-                                </span>
-                            }
-                        </>
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+  // console.log("current", currentMovieDetail);
+  if (isLoading) return <MovieDetailSkeleton />;
+  return (
+    <Stack
+      sx={{
+        width: "100%",
+        height: "",
+        flexDirection: "column",
+        alignItems: "center",
+        position: "relative",
+      }}
+    >
+      <BackDropImage
+        src={`https://image.tmdb.org/t/p/original${
+          currentMovieDetail ? currentMovieDetail.backdrop_path : ""
+        }`}
+      />
+      <Stack
+        className="header"
+        sx={{
+          position: "absolute",
+          top: { xs: "0", lg: "1rem" },
+          left: { xs: "0", lg: "1rem" },
+        }}
+      >
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <Typography
+            textAlign="center"
+            marginLeft="1rem"
+            fontWeight={800}
+            color="white"
+            fontSize={{ lg: "1.4rem" }}
+          >
+            Movie Central{" "}
+          </Typography>
+        </Link>
+      </Stack>
 
-export default Movie
+      <Stack
+        sx={{
+          position: { sm: "absolute" },
+          bottom: { sm: "10%", md: "10%" },
+          left: "10%",
+          paddingTop: { xs: "4rem", sm: "1.4rem" },
+          alignItems: "center",
+          display: { xs: "none", sm: "block" },
+        }}
+      >
+        <MovieCard isLoading={isLoading} movie={currentMovieDetail} />
+      </Stack>
+      <Stack
+        sx={{
+          alignItems: "flex-end",
+          justifyContent: "center",
+        }}
+      >
+        <Stack
+          alignItems={{ xs: "center", sm: "flex-start" }}
+          sx={{
+            maxWidth: {
+              xs: "100%",
+              sm: "60%",
+              lg: "70%",
+            },
+            padding: "1rem",
+          }}
+        >
+          <Typography variant="h4" fontWeight="600">
+            {currentMovieDetail ? currentMovieDetail.original_title : ""}
+          </Typography>
+          <Typography variant="p">
+            {currentMovieDetail ? currentMovieDetail.tagline : ""}
+          </Typography>
+
+          <Stack
+            lineHeight={"1rem"}
+            flexDirection="row"
+            gap=".5rem"
+            className="movie__rating"
+          >
+            <FcRating />
+            {currentMovieDetail
+              ? currentMovieDetail.vote_average
+              : ""}{" "}
+            <span className="movie__voteCount">
+              <FcLike />
+              {currentMovieDetail ? currentMovieDetail.vote_count : ""}
+            </span>
+          </Stack>
+          <Stack
+            sx={{
+              backgroundColor: "black",
+              width: "fit-content",
+              padding: ".4rem 1rem",
+              margin: ".6rem",
+              borderRadius: "10px",
+              marginLeft: "0",
+            }}
+            className="movie__runtime"
+          >
+            {currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}
+          </Stack>
+          <div className="movie__releaseDate">
+            {currentMovieDetail
+              ? "Release date: " +
+                format(new Date(currentMovieDetail.release_date), "PPP")
+              : ""}
+          </div>
+          <Box sx={{ margin: " 1.25rem 0", paddingTop: "1rem" }}>
+            {currentMovieDetail && currentMovieDetail.genres
+              ? currentMovieDetail.genres.map((genre) => (
+                  <>
+                    <span className="movie__genre" id={genre.id}>
+                      {genre.name}
+                    </span>
+                  </>
+                ))
+              : ""}
+          </Box>
+          <Stack sx={{ width: { lg: "50%", paddingTop: "2rem" } }}>
+            <Typography paragraph>
+              {currentMovieDetail ? currentMovieDetail.overview : ""}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MovieDetail;
